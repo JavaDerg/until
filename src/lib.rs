550a49e7c905a38301307e2607e44ed3803668e9
post-rex <post-rex@pm.me>
2020-11-20 14:09:30 +0100

    Documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
 use std::time::{Instant, Duration};
 
+/// Iter that will continue unless the inner iter is empty or a previously specified time has
+/// passed
 pub struct Until<I> {
 	inner: I,
 	until: Instant,
 }
 
 pub trait UntilExt<T: Iterator> {
-	fn until(self, until: Instant) -> Until<Self>
+	/// Using this the iter will continue till the specified time has passed or the internal
+    /// iter is empty
+    fn until(self, until: Instant) -> Until<Self>
 		where
 			Self: Sized,
 	{
 		Until::new(self, until)
 	}
 
+    /// Using this the iter will continue for the specified time or until the internal iter is
+    /// empty
 	fn do_for(self, until: Duration) -> Until<Self>
 		where
 			Self: Sized,
